832d16affdecf3a8748ecb87ff9fbd9c704fcf31
Missy Lovegren <audrey.lovegren@gmail.com>
2022-06-01 19:39:39 -0500

    Bind new endpoint handler to new bin button

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,3 +1,20 @@
 class Controller {
+  async addEndpoint() {
+    const endpoint = await this.model.addEndpoint();
+    this.view.onNewEndpointClick(endpoint);
+  }
+
+  #bindEvents() {
+    const newBinBtn = document.getElementById('newBinBtn');
+    newBinBtn.onclick = this.addEndpoint.bind(this);
+  }
+  
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+
+    this.#bindEvents();
+  }
 }
+
 export default Controller;
