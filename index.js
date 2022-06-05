175ba1f8cd9259831fb56e417a19cab9fcab8d14
Missy Lovegren <audrey.lovegren@gmail.com>
2022-06-05 13:47:03 -0500

    temporarily alter routes to talk directly to separate db services for testing

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import dbService from './data/services/dbService.js';
+// import dbService from './data/services/dbService.js';
+import { addRequestEntry, getRequestEntriesByEndpointId } from './public/data/mongoservice.js';
+import { addNewEndpoint, updateEndpoint } from './public/data/postgresserver.js';
 
 const app = express();
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-app.set('port', 3000);
+app.set('port', 3001);
 // app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // apis
-app.get('/api/endpoints/:endpoint_id', async (req, res) => {
-  try {
-    const details = await dbService.getEndpointDetails(
-      req.params.endpoint_id
-    );
+// app.get('/api/endpoints/:endpoint_id', async (req, res) => {
+//   try {
+//     // const details = await dbService.getEndpointDetails(
+//     //   req.params.endpoint_id
+//     // );
 
-    res.status(200).send(details);
-  } catch (error) {
-    res.status(404).json({ error });
-  }
-});
+//     const details = await
+
+//     res.status(200).send(details);
+//   } catch (error) {
+//     res.status(404).json({ error });
+//   }
+// });
 
 app.get('/api/endpoints/:endpoint_id/requests', async (req, res) => {
   try {
-    const requests = await dbService.getRequestEntriesByEndpointID(
+    // const requests = await dbService.getRequestEntriesByEndpointID(
+    //   req.params.endpoint_id
+    // );
+
+    const requests = await getRequestEntriesByEndpointId(
       req.params.endpoint_id
     );
 
@@ -43,7 +53,8 @@ app.get('/api/endpoints/:endpoint_id/requests', async (req, res) => {
 // POST requests for new endpoints
 app.post('/api/endpoints', async (req, res) => {
   try {
-    const endpointId = await dbService.createEndpoint();
+    // const endpointId = await dbService.createEndpoint();
+    const endpointId = await addNewEndpoint();
     res.status(201).json({ endpointId });
   } catch (error) {
     res.status(503).json({ error });
