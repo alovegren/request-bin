4d1cf5b12a82d30b8667baa40dbf5b2ae9cd4a07
Andrew Moore <andrewdmoore84@gmail.com>
2022-06-05 11:37:21 -0400

    Update mongoservice to use separate environmental variables with dotenv

--- public/data/mongoservice.js
+++ public/data/mongoservice.js
@@ -1,14 +1,8 @@
-// require('dotenv').config();
-// console.log(require('dotenv').config({ path: '../../.env' }));
-// const findConfig = require('find-config');
-// const dotenvPath = findConfig('.env');
-// require('dotenv').config({ path: dotenvPath });
-
+import dotenv from 'dotenv';
+dotenv.config();
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-// configuration info goes here
-
-const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@potatobin.z4lmi4p.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@potatobin.z4lmi4p.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -22,8 +16,8 @@ async function addRequestEntry({ requestMethod, requestIp, headers, payload, end
   try {
     await client.connect();
 
-    const db = client.db(`${MONGODB_DB_NAME}`);
-    const requestEntries = db.collection(`${MONGODB_COLLECTION_NAME}`);
+    const db = client.db(`${process.env.MONGODB_DB_NAME}`);
+    const requestEntries = db.collection(`${process.env.MONGODB_COLLECTION_NAME}`);
 
     newRequestEntry = await requestEntries.insertOne({
       requestMethod,
@@ -48,8 +42,8 @@ async function getRequestEntriesByEndpointId(id) {
   try {
     await client.connect();
 
-    const db = client.db(`${MONGODB_DB_NAME}`);
-    const requestEntries = db.collection(`${MONGODB_COLLECTION_NAME}`);
+    const db = client.db(`${process.env.MONGODB_DB_NAME}`);
+    const requestEntries = db.collection(`${process.env.MONGODB_COLLECTION_NAME}`);
 
     const matchingRequestEntriesCursor = await requestEntries.find({
       endpointId: id,
